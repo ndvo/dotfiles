5b12a86427747219f7ec5893dfd695ab71c2c94c
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-06-25 09:22:37 -0300

    install vim from source

---
+++ install-vim.sh
@@ -0,0 +1,24 @@
+cd apps
+git clone http://github.com/vim/vim.git
+cd vim/src
+./configure \
+            --with-features=huge \
+            --prefix=/usr/local \
+            --enable-gui=auto \
+           
+            --enable-cscope \
+            --enable-fail-if-missing \
+            --enable-largefile \
+            --enable-multibyte \
+            \
+            --enable-perlinterp=dynamic \
+            --enable-python3interp=dynamic \
+            --with-python3-config-dir='/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu' \
+            --enable-rubyinterp=dynamic \
+            --with-ruby-command=$(which ruby) \
+            --with-luajit \
+            --with-x \
+            \
+            --with-compiledby="stackoverflow.com/questions/3373914" 
+make
+make install
