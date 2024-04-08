eff7adb9f2a5bd4b45694a99ff793a8769593634
Nelson do Vale Oliveira <nelson@ocastudios.com>
2024-04-08 14:25:55 -0300

    Instalar: inclui um lembrete para instalar o delta

--- aptgets.sh
+++ aptgets.sh
@@ -1,3 +1,4 @@
 sudo apt install silversearcher-ag riprep \
   flowblade # video editor
   obs # video recorder 
+# Instalar também o delta para melhorar os diffs https://dandavison.github.io/delta/installation.html
