f39e51fb4e2e89f65f4861a25183654f1921c47b
Nelson Oliveira <nelson@ocastudios.com>
2026-09-11 13:17:34 -0300

    GH: sobrescreve o pr-description se o usuário aprovar

--- bin/get-pr-description.sh
+++ bin/get-pr-description.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-gh pr checkout "$1";
+branchName=$(git rev-parse --abbrev-ref HEAD);
+prFolders=".github/pr-descriptions";
+templateName="$branchName.template.md";
+templatePath="$prFolders/$templateName";
 
-gh pr view --json body -q '.body' > ".github/pr-descriptions/$(git rev-parse --abbrev-ref HEAD).md"
+fix_line_endings() {
+  sed 's/\r$//'
+}
 
-cat  ".github/pr-descriptions/$(git rev-parse --abbrev-ref HEAD).md"
-
-git checkout -
-
-
+if [ ! -f "$templatePath" ]; then
+  gh pr view --json body -q '.body' > "$templatePath"
+else
+  read -p "The template already exists. Do you want to overwrite it?" -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    gh pr view --json body -q '.body' | fix_line_endings > "$templatePath"
+  fi
+fi
